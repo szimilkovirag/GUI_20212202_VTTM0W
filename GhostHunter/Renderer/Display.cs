775f94b262326b4e25849e87b85654531ed369c1
Szimilkó Virág <szimiviri11@stud.uni-obuda.hu>
2022-03-31 21:09:53 +0200

    Revert "Revert "Starter background added""
    This reverts commit 1672f6759d157a59802e7bba49dafa0f2608750c.

---
+++ GhostHunter/Renderer/Display.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace GhostHunter.Renderer
+{
+    internal class Display:FrameworkElement
+    {
+        Size area;
+        public void SetupSizes(Size area)
+        {
+            this.area = area;
+            this.InvalidateVisual();
+        }
+
+        public Brush StarterBrush
+        {
+            get
+            {
+                return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Maps","starter_background.png"),UriKind.RelativeOrAbsolute)));
+            }
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            if(area.Width > 0 && area.Height > 0)
+            {
+                drawingContext.DrawRectangle(StarterBrush, null, new Rect(0, 0, area.Width, area.Height));
+            }
+        }
+    }
+}
