34af2850e0956ae568e22a05e8d162f4386c91b6
Szimilkó Virág <szimiviri11@stud.uni-obuda.hu>
2022-04-02 17:43:13 +0200

    Renderer fixed

--- GhostHunter/Renderer/Display.cs
+++ GhostHunter/Renderer/Display.cs
@@ -33,17 +33,15 @@ namespace GhostHunter.Renderer
                 return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Maps","starter_background.png"),UriKind.RelativeOrAbsolute)));
             }
         }
+
         protected override void OnRender(DrawingContext drawingContext)
         {
-            base.OnRender(drawingContext);
-            if(area.Width > 0 && area.Height > 0)
-            {
-                drawingContext.DrawRectangle(StarterBrush, null, new Rect(0, 0, area.Width, area.Height));
-            }
-
+            //base.OnRender(drawingContext);
             if(model!=null && area.Width > 0 && area.Height > 0)
             {
-                double rectWidth = ActualWidth / model.GameMatrix.GetLength(1);
+                drawingContext.DrawRectangle(StarterBrush, null, new Rect(0, 0, area.Width, area.Height));
+
+                double rectWidth = ActualWidth / model.GameMatrix.GetLength(0);
                 double rectHeight = ActualHeight / model.GameMatrix.GetLength(0);
 
                 for (int i = 0; i < model.GameMatrix.GetLength(0); i++)
@@ -54,23 +52,47 @@ namespace GhostHunter.Renderer
                         {
                             case GhostHunterLogic.MapItem.flower:
                                 drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "flower.png"), UriKind.RelativeOrAbsolute)))
-                                    , new Pen(Brushes.Black, 0), new Rect(i * rectWidth, j * rectHeight, 25, 30));
+                                    ,null, new Rect(j * rectWidth, i * rectHeight, 35, 35));
                                 break;
                             case GhostHunterLogic.MapItem.grass:
+                                drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "grass_mushroom_tree.png"), UriKind.RelativeOrAbsolute)))
+                                    , null, new Rect(j * rectWidth, i * rectHeight, 120, 90));
                                 break;
                             case GhostHunterLogic.MapItem.player:
+                                drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "cat.png"), UriKind.RelativeOrAbsolute)))
+                                    , null, new Rect(j * rectWidth, i * rectHeight, 40, 40));
                                 break;
                             case GhostHunterLogic.MapItem.tree2:
                                 drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "tree2.png"), UriKind.RelativeOrAbsolute)))
-                                    , new Pen(Brushes.Black, 0), new Rect(i * rectWidth, j * rectHeight, 100, 150));
+                                    , null, new Rect(j * rectWidth, i * rectHeight, 100, 150));
                                 break;
                             case GhostHunterLogic.MapItem.trees:
                                 drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "trees.png"), UriKind.RelativeOrAbsolute)))
-                                    , new Pen(Brushes.Black, 0), new Rect(i * rectWidth, j * rectHeight, 200, 180));
+                                    , null, new Rect(j * rectWidth, i * rectHeight, 200, 180));
                                 break;
-                            case GhostHunterLogic.MapItem.signboard:
-                                drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "signboard.png"), UriKind.RelativeOrAbsolute)))
-                                    , new Pen(Brushes.Black, 0), new Rect(i * rectWidth, j * rectHeight, 45, 50));
+                            case GhostHunterLogic.MapItem.woods:
+                                drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "woods.png"), UriKind.RelativeOrAbsolute)))
+                                    , null, new Rect(j * rectWidth, i * rectHeight, 45, 50));
+                                break;
+                            case GhostHunterLogic.MapItem.winter:
+                                drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "winter.png"), UriKind.RelativeOrAbsolute)))
+                                    , null, new Rect(j * rectWidth, i * rectHeight, 45, 50));
+                                break;
+                            case GhostHunterLogic.MapItem.desert:
+                                drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "desert.png"), UriKind.RelativeOrAbsolute)))
+                                    , null, new Rect(j * rectWidth, i * rectHeight, 45, 50));
+                                break;
+                            case GhostHunterLogic.MapItem.rocks:
+                                drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "grass_rocks.png"), UriKind.RelativeOrAbsolute)))
+                                    , null, new Rect(j * rectWidth, i * rectHeight, 110, 65));
+                                break;
+                            case GhostHunterLogic.MapItem.mushroom:
+                                drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "rocks_mushrooms.png"), UriKind.RelativeOrAbsolute)))
+                                    , null, new Rect(j * rectWidth, i * rectHeight, 120, 90));
+                                break;
+                            case GhostHunterLogic.MapItem.tree1:
+                                drawingContext.DrawRectangle(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Elements", "tree1.png"), UriKind.RelativeOrAbsolute)))
+                                    , null, new Rect(j * rectWidth, i * rectHeight, 35, 35));
                                 break;
                             case GhostHunterLogic.MapItem.ground:
                                 break;
