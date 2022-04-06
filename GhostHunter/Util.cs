3abfbcfef0812f7e6a91c22872980b31875f0073
GombarEgon <gombar.egi@gmail.com>
2022-04-06 22:32:50 +0200

    Added random generator class

---
+++ GhostHunter/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostHunter
+{
+    public static class Util
+    {
+        public static Random r;
+        static Util()
+        {
+            r = new Random();
+        }
+    }
+}
