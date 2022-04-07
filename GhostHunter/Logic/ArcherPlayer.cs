dcc2fdf419e02204fa44210695228fe74eb8247a
GombarEgon <gombar.egi@gmail.com>
2022-04-07 10:34:13 +0200

    Modified Logic with Players

---
+++ GhostHunter/Logic/ArcherPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostHunter.Logic
+{
+    public class ArcherPlayer : Player
+    {
+        public ArcherPlayer(int player_i, int player_j) : base(player_i, player_j)
+        {
+        }
+    }
+}
---
+++ GhostHunter/Logic/AttackerPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostHunter.Logic
+{
+    public class AttackerPlayer : Player
+    {
+        public AttackerPlayer(int player_i, int player_j) : base(player_i, player_j)
+        {
+        }
+    }
+}
